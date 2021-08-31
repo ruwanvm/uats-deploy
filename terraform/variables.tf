474ae570718ec5a90596561eb75425647da9b423
ruwanvm <ruwanvm@gmail.com>
2021-08-31 17:41:43 +0530

    Dynamodb deployment test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,5 +34,5 @@ variable "APP_VERSION" {
 locals {
   cluster_name = "${var.environment}-eks-cluster"
   env_name_gen2 = "${var.environment}_v2"
-  dynamodb_endpoint = "dynamodb.${var.environment}.amazonaws.com"
+  dynamodb_endpoint = "dynamodb.${var.AWS_REGION}.amazonaws.com"
 }
