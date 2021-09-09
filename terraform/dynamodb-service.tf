a8f87ac05da49537616f0f4ec88443f4c7edd164
ruwanvm <ruwanvm@gmail.com>
2021-09-09 23:45:00 +0530

    Parakum microservice deployment

--- terraform/dynamodb-service.tf
+++ terraform/dynamodb-service.tf
@@ -1,17 +1,17 @@
-resource "kubernetes_service" "dynamodb" {
-  # This will used when the DYNAMODB_HOST environment variable is set on deployment
-  metadata {
-    name      = "dynamodb"
-    namespace = kubernetes_namespace.namespace.id
-  }
-
-  spec {
-    type          = "ExternalName"
-    external_name = local.dynamodb_endpoint
-    port {
-      name = "https-port"
-      port = 443
-    }
-  }
-}
-
+//resource "kubernetes_service" "dynamodb" {
+//  # This will used when the DYNAMODB_HOST environment variable is set on deployment
+//  metadata {
+//    name      = "dynamodb"
+//    namespace = kubernetes_namespace.namespace.id
+//  }
+//
+//  spec {
+//    type          = "ExternalName"
+//    external_name = local.dynamodb_endpoint
+//    port {
+//      name = "https-port"
+//      port = 443
+//    }
+//  }
+//}
+//
